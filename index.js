0da77a49f1916e2cbc7de78a46095a2f8b220d69
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-07-26 14:40:37 -0400

    nosleep order

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //Hot reload fix
 document.body.innerHTML = "";
 require("./css/main.scss");
-
+require("./output/Main").main();
 var NoSleep = require("nosleep.js");
 var noSleep = new NoSleep();
 noSleep.enable();
-
-require("./output/Main").main();
