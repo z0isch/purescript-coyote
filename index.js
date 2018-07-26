8b050460703c7fd6e2f2e7532c9fb26811e5ff66
BELLARMINE\aruf <aruf@bellarmine.edu>
2018-07-26 16:35:01 -0400

    noSleep try

--- index.js
+++ index.js
@@ -2,16 +2,5 @@
 if (document.getElementById("coyote") !== null) {
   document.getElementById("coyote").innerHTML = "";
 }
-
-function enableNoSleep() {
-  var NoSleep = require("nosleep.js");
-  var noSleep = new NoSleep();
-  noSleep.enable();
-  document.removeEventListener("touchstart", enableNoSleep, false);
-  document.removeEventListener("click", enableNoSleep, false);
-}
-document.addEventListener("click", enableNoSleep, false);
-document.addEventListener("touchstart", enableNoSleep, false);
-
 require("./css/main.scss");
 require("./output/Main").main();
